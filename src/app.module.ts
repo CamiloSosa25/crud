05e95c0247900232f79619f9ffab2145003c4ad5
csosa <dir.innovacion@innovartecing.com>
2024-02-10 13:08:54 -0500

    Add employee module and validation pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
+import { EmployeeModule } from './employee/employee.module';
 
 @Module({
-  imports: [],
+  imports: [EmployeeModule],
+  providers: [{
+    provide: APP_PIPE,
+    useClass: ValidationPipe,
+  }],
 })
 export class AppModule { }
