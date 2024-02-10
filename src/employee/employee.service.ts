c2505358f89ceaa5c968ea2e9541778faa809a7f
csosa <dir.innovacion@innovartecing.com>
2024-02-10 12:42:14 -0500

    Add CRUD operations to EmployeeService

--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@nestjs/common";
+import { Employee } from "@prisma/client";
 import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
@@ -8,7 +9,30 @@ export class EmployeeService {
 
     }
 
-    getAll() {
+    getAll(): Promise<Employee[]> {
         return this.prisma.employee.findMany();
+    }
+
+    getById(id: number): Promise<Employee> {
+        return this.prisma.employee.findUnique({
+            where: {
+                id: id
+            }
+        });
+    }
+
+    create(data: Employee): Promise<Employee> {
+        return this.prisma.employee.create({
+            data: data
+        });
+    }
+
+    update(id: number, data: Employee): Promise<Employee> {
+        return this.prisma.employee.update({
+            where: {
+                id: id
+            },
+            data: data
+        });
     }
 }
