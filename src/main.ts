d1005a5a26ad6123a49fb851984ac56d05ec9f56
csosa <dir.innovacion@innovartecing.com>
2024-02-10 14:20:14 -0500

    Add Swagger documentation and validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Crud')
+    .setDescription('Payroll Management System API')
+    .setVersion('1.0')
+    .addTag('employee')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
